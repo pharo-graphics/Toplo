c63a7d84d4f325120bd49f09d3bcae229aaa8dec
Alain Plantec <alain.plantec@univ-brest.fr>
2024-06-05 00:09:13 +0200

    + build.sh and ciPreLoading.st

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+
+# Define SCRIPTS_DIR and REPO_DIR in a way this script can
+# be executed:
+#   * from any directory
+#   * from linux, macos, and windows (with mingw64)
+case "$(uname -s)" in
+   MINGW*)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -W )"
+     ;;
+   *)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+     ;;
+esac
+REPO_DIR="$(dirname "$SCRIPTS_DIR")"
+
+# Get a fresh image and load code
+curl https://get.pharo.org/64/110+vm | bash
+./pharo Pharo.image eval --save "EpMonitor current disable"
+./pharo Pharo.image metacello install "tonel://$REPO_DIR/src" BaselineOfToplo
+./pharo Pharo.image eval --save "EpMonitor current enable"
